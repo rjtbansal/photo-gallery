ec118ffbf8942082b8a78e2da5dc7a15a97e2c16
Rajat Bansal <Rajat_Bansal@Trimble.com>
2016-10-27 12:27:50 -0700

    edited files as per requirement

--- admin/photos.php
+++ admin/photos.php
@@ -1,10 +1,24 @@
- <div class="container-fluid">
+<?php include("includes/header.php"); ?>
+
+        <!-- Navigation -->
+        <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <?php include("includes/topNav.php"); ?>
+
+            <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
+            <?php include("includes/sidebar.php"); ?>            
+
+            <!-- /.navbar-collapse -->
+        </nav>
+
+        <div id="page-wrapper">
+            <div class="container-fluid">
 
                 <!-- Page Heading -->
                 <div class="row">
                     <div class="col-lg-12">
                         <h1 class="page-header">
-                            Admin
+                            Photos
                             <small>Subheading</small>
                         </h1>
                         <ol class="breadcrumb">
@@ -20,4 +34,8 @@
                 <!-- /.row -->
 
             </div>
-            <!-- /.container-fluid -->
+    
+        </div>
+        <!-- /#page-wrapper -->
+
+  <?php include("includes/footer.php"); ?>
