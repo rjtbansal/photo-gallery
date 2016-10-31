4c7689d4e0101945353d33cca25656b25e95ac68
rjtbansal <rjtbansal@yahoo.com>
2016-10-30 18:59:08 -0700

    added seperate function to execute queries in users

--- admin/includes/user.php
+++ admin/includes/user.php
@@ -2,17 +2,20 @@
 
 class User{
     //making it static so that we dont have to instantiate it every time from outside
-    public static function getAllUsers(){
-        global $database;
-        $result=$database->query("SELECT * from users");
-        return $result;
+    public static function getAllUsers(){      
+        return self::execute_query("SELECT * from users");
     }
 
     public static function getUserById($id){
-        global $database;
-        $result=$database->query("SELECT * from users where id='{$id}' LIMIT 1");
+        $result=self::execute_query("SELECT * from users where id='{$id}' LIMIT 1");
         $get_user=mysqli_fetch_array($result);
         return $get_user;
+    }
+
+    public static function execute_query($query){
+        global $database;
+        $result=$database->query($query);
+        return $result;
     }
 }
 
